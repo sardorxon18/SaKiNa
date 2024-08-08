2acdcb8ee53d5a8b208fdef396e2eb006c28209d
Anonimous <134620613+sardorxon18@users.noreply.github.com>
2024-08-08 05:29:28 +0500

    Herocu Uchun o'zgartirishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-u5a2sp5e9byf3ur3-lt-bv9pm8=9glmj^isw+4iqlttq2k0tc@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,9 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- pagesapp/tests.py
+++ pagesapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from django.test import SimpleTestCase
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/simple/')
+        self.assertEqual(response.status_code,200)
+    def test_about_page_status_code(self):
+        response = self.client.get('/salom/')
+        self.assertEqual(response.status_code,200)
+    def test_about_page_status_code(self):
+        response = self.client.get('/htmlandcss/')
+        self.assertEqual(response.status_code,200)
+    def test_about_page_status_code(self):
+        response = self.client.get('/basic/')
+        self.assertEqual(response.status_code,200)
